fe9d3c9af92ccd26850f8d46fe9c6bc5660506a9
AndroidKuma <242420648+AndroidKumaCodes@users.noreply.github.com>
2026-04-10 02:28:16 +0200

    Add getUser to UserRepository interface

--- app/src/main/java/com/akcrba/smartstep/feature/myprofile/domain/reposiroty/UserRepository.kt
+++ app/src/main/java/com/akcrba/smartstep/feature/myprofile/domain/reposiroty/UserRepository.kt
@@ -1,8 +1,10 @@
 package com.akcrba.smartstep.feature.myprofile.domain.reposiroty
 
 import com.akcrba.smartstep.feature.myprofile.domain.model.User
+import kotlinx.coroutines.flow.Flow
 
 internal interface UserRepository {
+    fun getUser(): Flow<User?>
     suspend fun saveUser(user: User)
     suspend fun deleteUser()
 }
