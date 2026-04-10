af88f35ab6872abf931d03f508858a95effa467b
AndroidKuma <242420648+AndroidKumaCodes@users.noreply.github.com>
2026-04-10 02:19:59 +0200

    Correct database queries

--- app/src/main/java/com/akcrba/smartstep/feature/myprofile/data/local/dao/UserDao.kt
+++ app/src/main/java/com/akcrba/smartstep/feature/myprofile/data/local/dao/UserDao.kt
@@ -5,14 +5,19 @@ import androidx.room3.Insert
 import androidx.room3.OnConflictStrategy
 import androidx.room3.Query
 import com.akcrba.smartstep.feature.myprofile.data.local.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
 import org.koin.core.annotation.Singleton
 
 @Dao
 @Singleton
 internal interface UserDao {
+
+    @Query("SELECT * FROM users WHERE id = 1")
+    fun getUser(): Flow<UserEntity?>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUser(userEntity: UserEntity)
 
-    @Query("DELETE FROM users WHERE id = :id")
-    suspend fun deleteUser(id: Int = 1)
+    @Query("DELETE FROM users WHERE id = 1")
+    suspend fun deleteUser()
 }
