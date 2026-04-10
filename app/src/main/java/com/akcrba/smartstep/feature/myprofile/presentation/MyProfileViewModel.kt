8560f1ea2f07812c4d1ab0ae895250b45dbe6754
AndroidKuma <242420648+AndroidKumaCodes@users.noreply.github.com>
2026-04-10 02:32:40 +0200

    Add init block + define OnClickDeleteUser, OnClickSkip, and OnClickStart

--- app/src/main/java/com/akcrba/smartstep/feature/myprofile/presentation/MyProfileViewModel.kt
+++ app/src/main/java/com/akcrba/smartstep/feature/myprofile/presentation/MyProfileViewModel.kt
@@ -1,32 +1,78 @@
 package com.akcrba.smartstep.feature.myprofile.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.akcrba.smartstep.feature.myprofile.domain.usecase.DeleteUserUseCase
+import com.akcrba.smartstep.feature.myprofile.domain.usecase.GetUserUseCase
 import com.akcrba.smartstep.feature.myprofile.domain.usecase.SaveUserUseCase
 import com.akcrba.smartstep.feature.myprofile.presentation.model.BodyStats
 import com.akcrba.smartstep.feature.myprofile.presentation.model.DialogType
+import com.akcrba.smartstep.feature.myprofile.presentation.model.Height
 import com.akcrba.smartstep.feature.myprofile.presentation.model.MyProfileItems
+import com.akcrba.smartstep.feature.myprofile.presentation.model.UiUser
+import com.akcrba.smartstep.feature.myprofile.presentation.model.Weight
+import com.akcrba.smartstep.feature.myprofile.presentation.model.toDomainUser
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import org.koin.core.annotation.KoinViewModel
+import kotlin.math.roundToInt
 
 @KoinViewModel
 internal class MyProfileViewModel(
-    internal val saveUserUseCase: SaveUserUseCase,
-    internal val deleteUserUseCase: DeleteUserUseCase,
+    getUserUseCase: GetUserUseCase,
+    private val saveUserUseCase: SaveUserUseCase,
+    private val deleteUserUseCase: DeleteUserUseCase,
 ) : ViewModel() {
     private val _state = MutableStateFlow(MyProfileScreenState())
     val state: StateFlow<MyProfileScreenState> = _state.asStateFlow()
 
     internal val myProfileItems = MyProfileItems()
 
+    init {
+        getUserUseCase().onEach { domainUser ->
+            _state.update { currentState ->
+                when (domainUser) {
+                    null -> if (currentState.user == UiUser()) currentState else currentState.copy(user = UiUser()) // Safely reset to defaults
+                    else -> currentState.copy(
+                        user = UiUser(
+                            gender = domainUser.gender,
+                            bodyStats = BodyStats(
+                                height = Height.fromMetric(domainUser.height.roundToInt()),
+                                weight = Weight.fromMetric(domainUser.weight.roundToInt()),
+                                isMetric = domainUser.isMetric,
+                            ),
+                        ),
+                    )
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
     internal fun onAction(action: MyProfileAction) {
         when (action) {
-            MyProfileAction.OnClickDeleteUser -> {} // TODO
-            MyProfileAction.OnClickSkip -> {} // TODO
-            MyProfileAction.OnClickStart -> {} // TODO
+            MyProfileAction.OnClickDeleteUser -> {
+                viewModelScope.launch {
+                    deleteUserUseCase()
+                }
+            }
+
+            MyProfileAction.OnClickSkip -> {
+                viewModelScope.launch {
+                    val user = UiUser()
+                    saveUserUseCase(user.toDomainUser())
+                }
+            }
+            MyProfileAction.OnClickStart -> {
+                viewModelScope.launch {
+                    saveUserUseCase(state.value.user.toDomainUser())
+                }
+            }
+
             is MyProfileAction.SetGender -> _state.update { state ->
                 state.copy(user = state.user.copy(gender = action.gender))
             }
