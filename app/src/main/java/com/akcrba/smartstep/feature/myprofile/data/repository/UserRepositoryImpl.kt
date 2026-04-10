c58fa9f9974f0e65b2eaa5fd4cc13fceddca171d
AndroidKuma <242420648+AndroidKumaCodes@users.noreply.github.com>
2026-04-10 02:27:34 +0200

    Add getUser() to UserRepositoryImpl

--- app/src/main/java/com/akcrba/smartstep/feature/myprofile/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/akcrba/smartstep/feature/myprofile/data/repository/UserRepositoryImpl.kt
@@ -2,14 +2,23 @@ package com.akcrba.smartstep.feature.myprofile.data.repository
 
 import com.akcrba.smartstep.feature.myprofile.data.local.dao.UserDao
 import com.akcrba.smartstep.feature.myprofile.data.local.entity.toEntity
+import com.akcrba.smartstep.feature.myprofile.data.local.entity.toUser
 import com.akcrba.smartstep.feature.myprofile.domain.model.User
 import com.akcrba.smartstep.feature.myprofile.domain.reposiroty.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import org.koin.core.annotation.Singleton
 
 @Singleton
 internal class UserRepositoryImpl(
     private val userDao: UserDao,
 ) : UserRepository {
+
+    override fun getUser(): Flow<User?> {
+        return userDao.getUser().map { userEntity ->
+            userEntity?.toUser()
+        }
+    }
 
     override suspend fun saveUser(user: User) {
         userDao.insertUser(user.toEntity())
