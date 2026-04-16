9ed64314b6b1686a5afc4f988a638735db51271b
AndroidKuma <242420648+AndroidKumaCodes@users.noreply.github.com>
2026-04-16 16:42:49 +0200

    Create HomeContract holding state and action intent

---
+++ app/src/main/java/com/akcrba/smartstep/feature/home/presentation/HomeContract.kt
@@ -0,0 +1,33 @@
+package com.akcrba.smartstep.feature.home.presentation
+
+internal data class HomeScreenState(
+    val steps: Int = 0,
+    val goal: Int = 6000,
+    val showFixStopCountingStepsIssue: Boolean = false,
+)
+
+internal sealed interface HomeAction {
+
+    sealed interface HamburgerMenu : HomeAction {
+        data object OnClickHamburgerMenu : HomeAction
+        data object OnClickFixStopCountingStepsIssue : HamburgerMenu
+        data object OnClickStepGoal : HamburgerMenu
+        data object OnClickPersonalSettings : HamburgerMenu
+        data object OnClickExit : HamburgerMenu
+    }
+
+    sealed interface StepGoal : HomeAction {
+        data object onClickSave : StepGoal
+        data object onClickCancel : StepGoal
+    }
+
+    sealed interface BottomSheet : HomeAction {
+        data object OnClickAllowAccess : BottomSheet
+        data object OnClickOpenSettings : BottomSheet
+        data object OnClickContinue : BottomSheet
+    }
+
+    sealed interface ExitDialog : HomeAction {
+        data object OnClickOk : ExitDialog
+    }
+}
